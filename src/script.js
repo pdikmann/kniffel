e951b85cf541e546b4a7fd446215b86b6857ab6e
Philipp Dikmann <mail@philippdikmann.de>
2020-12-17 03:21:13 +0100

    start work on multiplayer

--- src/script.js
+++ src/script.js
@@ -8,7 +8,7 @@
 window.onload = () => {
   runAllTests()
   getDOM(dom)
-  makeTable(dom.scoreboard)
+  makeTable2(dom.scoreboard)
   rollAll(state)
   render(state)
 }
@@ -29,12 +29,17 @@ let TurnState = {
 
 let state = {
   currentPlayer: 0,
-  playerCount: 1,
+  playerCount: 4,
   dice: [],
-  score: [], // per player
+  score: [
+    [],
+    [],
+    [],
+    []
+  ], // per player
   turnState: TurnState.FirstRoll,
-  bonus: 0,
-  bonusFlag: 0 // -1 for fail, 1 for success
+  bonus: [0, 0, 0, 0],
+  bonusFlag: [0, 0, 0, 0] // -1 for fail, 1 for success
 }
 
 function advanceTurn(state) {
@@ -96,19 +101,19 @@ ui.keep = (n) => {
   render(state)
 }
 
-ui.selectMatch = (n) => {
+ui.selectMatch = (n, pi) => {
   if (state.turnState == TurnState.FirstRoll) return
-  let match = dom.matches[n]
+  let match = dom.matches[pi][n]
   if (match.done) return
   match.done = true
-  state.score[n] = matches[n].fn(diceValues(state))
-  if (state.bonusFlag == 0) {
-    if (state.score.slice(0, 6).filter(x => x == 0).length > 0) {
-      state.bonusFlag = -1
+  state.score[pi][n] = matches[n].fn(diceValues(state))
+  if (state.bonusFlag[pi] == 0) {
+    if (state.score[pi].slice(0, 6).filter(x => x == 0).length > 0) {
+      state.bonusFlag[pi] = -1
     }
-    if (state.score.slice(0, 6).filter(x => x > 0).length == 6) {
-      state.bonusFlag = 1
-      state.bonus = 35
+    if (state.score[pi].slice(0, 6).filter(x => x > 0).length == 6) {
+      state.bonusFlag[pi] = 1
+      state.bonus[pi] = 35
     }
   }
   unkeep(state)
@@ -127,10 +132,10 @@ let dom = {
   rollButton: {},
   scoreboard: {},
   matches: [],
-  bonus: {},
-  sumTop: {},
-  sumBottom: {},
-  sum: {}
+  bonus: [],
+  sumTop: [],
+  sumBottom: [],
+  sum: []
 }
 
 function getDOM(dom) {
@@ -146,75 +151,133 @@ function render(state) {
     dices[d].textContent = state.dice[d].value
     dices[d].className = "dice" + (state.dice[d].keep ? " keep" : "")
   }
-  for (var m = 0; m < dom.matches.length; m++) {
-    let match = dom.matches[m].element,
-      oldScore = state.score[m],
-      score = (oldScore == undefined) ? matches[m].fn(diceValues(state)) : oldScore
-
-    if (score == 0) {
-      match.className = "zero"
-    } else {
-      match.className = "ok"
+  for (var pi = 0; pi < state.playerCount; pi++) {
+    for (var m = 0; m < dom.matches[pi].length; m++) {
+      let match = dom.matches[pi][m].element,
+        oldScore = state.score[pi][m],
+        score = (oldScore == undefined) ? matches[m].fn(diceValues(state)) : oldScore
+      if (score == 0) {
+        match.className = "zero"
+      } else {
+        match.className = "ok"
+      }
+      if (dom.matches[pi][m].done) {
+        addClass(match, "done")
+      }
+      match.textContent = score
+      if (state.turnState == TurnState.FirstRoll && !dom.matches[pi][m].done) {
+        match.textContent = ""
+        match.className = ""
+      }
     }
-    if (dom.matches[m].done) {
-      addClass(match, "done")
-      // continue
+    if (state.bonusFlag[pi] > 0) {
+      dom.bonus[pi].textContent = state.bonus[pi]
+      dom.bonus[pi].className = "ok done"
     }
-    match.textContent = score
-    if (state.turnState == TurnState.FirstRoll && !dom.matches[m].done) {
-      match.textContent = ""
-      match.className = ""
+    if (state.bonusFlag[pi] < 0) {
+      dom.bonus[pi].textContent = state.bonus[pi]
+      dom.bonus[pi].className = "zero done"
     }
+    dom.sumTop[pi].textContent = state.score[pi].slice(0, 6).filter(identity).reduce(sum, 0) + state.bonus[pi]
+    dom.sumBottom[pi].textContent = state.score[pi].slice(6).filter(identity).reduce(sum, 0)
+    dom.sum[pi].textContent = state.score[pi].filter(identity).reduce(sum, 0) + state.bonus[pi]
   }
-  if (state.bonusFlag > 0) {
-    dom.bonus.textContent = state.bonus
-    dom.bonus.className = "ok done"
-  }
-  if (state.bonusFlag < 0) {
-    dom.bonus.textContent = state.bonus
-    dom.bonus.className = "zero done"
-  }
-  dom.sumTop.textContent = state.score.slice(0, 6).filter(identity).reduce(sum, 0) + state.bonus
-  dom.sumBottom.textContent = state.score.slice(6).filter(identity).reduce(sum, 0)
-  dom.sum.textContent = state.score.filter(identity).reduce(sum,0) + state.bonus
 }
 
-function makeTable(parent) {
+function makeTable2(parent) {
   let table = mk.table()
-  let tr = mk.tr(table)
-  mk.td(tr, "Spiel")
-  mk.td(tr, "Spieler 1")
-  for (var i = 0; i < 6; i++) { // first 6 matches (top)
-    makeMatch(table, matches[i], i)
-  }
-  tr = mk.tr(table)
-  mk.td(tr, "Bonus")
-  dom.bonus = mk.td(tr, 0)
-  tr = mk.tr(table)
-  mk.td(tr, "Summe Oben")
-  dom.sumTop = mk.td(tr, 0)
-  for (var i = 6; i < matches.length; i++) { // rest of matches (bottom)
-    makeMatch(table, matches[i], i)
-  }
-  tr = mk.tr(table)
-  mk.td(tr, "Summe Unten")
-  dom.sumBottom = mk.td(tr, 0)
-  tr = mk.tr(table)
-  mk.td(tr, "Summe")
-  dom.sum = mk.td(tr, 0)
+  let trs = []
+  trs.push("Kategorie")
+  for (var i = 0; i < 6; i++) trs.push(matches[i].label)
+  trs.push("Bonus", "Summe Oben")
+  for (var i = 6; i < matches.length; i++) trs.push(matches[i].label)
+  trs.push("Summe Unten", "Summe")
+  let players = [],
+    tds = []
+  for (var i = 0; i < state.playerCount; i++) players.push(i)
+  // make [] of all rows
+  // then make [] of all match functions
+  // then zip it up
+  players.forEach((_, pi) => {
+    dom.bonus[pi] = {}
+    dom.sumTop[pi] = {}
+    dom.sumBottom[pi] = {}
+    dom.sum[pi] = {}
+    dom.matches[pi] = matches.map(_ => {})
+    let td = []
+    td.push((tr) => mk.td(tr, `Spieler ${pi}`))
+    for (var m1 = 0; m1 < 6; m1++) mishMatch(td, m1, pi)
+    td.push((tr) => dom.bonus[pi] = mk.td(tr, 0))
+    td.push((tr) => dom.sumTop[pi] = mk.td(tr, 0))
+    for (var m2 = 6; m2 < matches.length; m2++) mishMatch(td, m2, pi)
+    td.push((tr) => dom.sumBottom[pi] = mk.td(tr, 0))
+    td.push((tr) => dom.sum[pi] = mk.td(tr, 0))
+    tds.push(td)
+  })
+  trs.forEach((row, i) => {
+    let tr = mk.tr(table)
+    mk.td(tr, row)
+    players.forEach((player, j) => {
+      tds[j][i](tr)
+    })
+  })
   parent.appendChild(table)
 }
 
-function makeMatch(parent, match, n) {
-  tr = mk.tr(parent)
-  mk.td(tr, match.label)
-  let m = mk.td(tr, "-_-")
-  m.addEventListener("click", () => ui.selectMatch(n))
-  dom.matches.push({
-    element: m,
-    done: false,
+function mishMatch(td, mi, pi) {
+  td.push((tr) => {
+    let m = mk.td(tr, "UwU")
+    m.addEventListener("click", () => ui.selectMatch(mi, pi))
+    dom.matches[pi][mi] = {
+      element: m,
+      done: false
+    }
   })
 }
+
+// function makeTd2(parent, txt) {
+//   let dom = mk.td(parent, txt)
+//   return
+// }
+
+// function makeTable(parent) {
+//   let table = mk.table()
+//   let tr = mk.tr(table)
+//   mk.td(tr, "Kategorie")
+//   for (var i = 0; i < state.playerCount; i++) {
+//     mk.td(tr, `Spieler ${i}`)
+//   }
+//   for (var i = 0; i < 6; i++) { // first 6 matches (top)
+//     makeMatch(table, matches[i], i)
+//   }
+//   tr = mk.tr(table)
+//   mk.td(tr, "Bonus")
+//   dom.bonus = mk.td(tr, 0)
+//   tr = mk.tr(table)
+//   mk.td(tr, "Summe Oben")
+//   dom.sumTop = mk.td(tr, 0)
+//   for (var i = 6; i < matches.length; i++) { // rest of matches (bottom)
+//     makeMatch(table, matches[i], i)
+//   }
+//   tr = mk.tr(table)
+//   mk.td(tr, "Summe Unten")
+//   dom.sumBottom = mk.td(tr, 0)
+//   tr = mk.tr(table)
+//   mk.td(tr, "Summe")
+//   dom.sum = mk.td(tr, 0)
+//   parent.appendChild(table)
+// }
+//
+// function makeMatch(parent, match, n) {
+//   tr = mk.tr(parent)
+//   mk.td(tr, match.label)
+//   let m = mk.td(tr, "-_-")
+//   m.addEventListener("click", () => ui.selectMatch(n))
+//   dom.matches.push({
+//     element: m,
+//     done: false,
+//   })
+// }
 
 function addClass(dom, className) {
   dom.className = stringAdd(dom.className, className)
